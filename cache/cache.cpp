7cd64e1f858941adead69119d866a252b20defba
Wei Song <wsong83@gmail.com>
2019-01-23 16:44:32 +0800

    play with the calibration

---
+++ cache/cache.cpp
@@ -0,0 +1,61 @@
+#include "cache/cache.hpp"
+#include "util/statistics.hpp"
+
+#include <iomanip>
+#include <fstream>
+
+int calibrate(char *victim) {
+  uint32_t repeat = 500;
+  double unflushed = 0.0;
+  double flushed = 0.0;
+  uint32_t stat_histo_unflushed = init_histo_stat(20, repeat);
+  uint32_t stat_histo_flushed = init_histo_stat(20, repeat);
+  
+  maccess (victim);
+  maccess (victim);
+  maccess (victim);
+  maccess (victim);
+
+  for (int i=0; i < repeat; i++) {
+    maccess (victim);
+    maccess (victim);
+    maccess (victim);
+    maccess (victim);
+
+    uint64_t time = clock_thread();
+    maccess (victim);
+    uint64_t delta = clock_thread() - time;
+    record_histo_stat(stat_histo_unflushed, (double)(delta));
+  }
+
+  for (i=0; i < repeat; i++) {
+    maccess (victim);
+    maccess (victim);
+    maccess (victim);
+    maccess (victim);
+
+    flush (victim);
+    uint64_t time = clock_thread();
+    maccess (victim);
+    uint64_t delta = clock_thread() - time;
+    record_histo_stat(stat_histo_flushed, (double)(delta));
+  }
+
+  {
+    std::ofstream outfile("unflushed.data", std::ofstream::app);
+    auto hist = get_histo_density(stat_histo_unflushed);
+    for(uint32_t i=0; i<hist.size(); i++)
+      outfile << hist[i].first << "\t0\t0\t" << hist[i].second << "\t0" << std::endl;
+    outfile.close();
+  }
+
+  {
+    std::ofstream outfile("flushed.data", std::ofstream::app);
+    auto hist = get_histo_density(stat_histo_flushed);
+    for(uint32_t i=0; i<hist.size(); i++)
+      outfile << hist[i].first << "\t0\t0\t" << hist[i].second << "\t0" << std::endl;
+    outfile.close();
+  }
+  
+  return 0;
+}
---
+++ cache/cache.hpp
@@ -0,0 +1,17 @@
+#ifndef SCE_CACHE_HPP
+#define SCE_CACHE_HPP
+
+#define MSZ_CL 56
+#define MSZ_PG 4088
+
+typedef struct elem {
+  struct elem *next;
+  struct elem *prev;
+  int set;      // >0 : set id; -1: picked; -2: initialized
+  unsigned int delta;
+} elem_t;
+
+// get the latency threshold
+int calibrate(char *victim); 
+
+#endif
