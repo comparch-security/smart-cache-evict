7a9df6e83a57b303d5d46c67e2e4301757d640b5
Wei Song <wsong83@gmail.com>
2019-03-06 17:58:41 +0800

    update combine alg

--- test/test-evict-combine.cpp
+++ test/test-evict-combine.cpp
@@ -2,43 +2,63 @@
 #include "cache/algorithm.hpp"
 #include "cache/list.hpp"
 #include <cstdio>
+#include <chrono>
 
 int main() {
   init_cfg();
   randomize_seed();
   init_threads();
-  int way = 32;
+  int way = CFG.cache_way;
   int succ = 0, iter = 0, keep = 0;
-  int csize = 120000;
-  int way_pre = way;
-  while (keep < 5 && iter < 200) {
+  int csize = 224000;
+  std::chrono::high_resolution_clock::time_point tb1, tb2, tend;
+  long int time_all_acc = 0, time_trim_acc = 0;
+  while (iter < 100) {
     elem_t *victim = allocate_list(1);
-    elem_t *candidate = NULL;
     calibrate(victim);
-    bool rv = trim_tar_combined_ran(&candidate, victim, way, csize, 600, 21);
-    int m_way = way;
-    if(rv) {
-      way = trim_tar_final(&candidate, victim);
-      rv = test_tar_pthread(candidate, victim);
-      printf("verify result %d way = %d\n", rv, way);
-    }
-    free_list(candidate);
+    int ct = 0;
+    elem_t *candidate = NULL, *residue = NULL;
+    bool rv = false;
+    tb1 = std::chrono::high_resolution_clock::now();
+    while(ct++ < 9) {
+      if(candidate == NULL) {
+        candidate = allocate_list(csize);
+        while(!test_tar_pthread(candidate, victim)) {
+          free_list(candidate);
+          candidate = allocate_list(csize);
+        }
+      }
+      tb2 = std::chrono::high_resolution_clock::now();
+      rv = trim_tar_ran(&candidate, victim, way);
+      tend = std::chrono::high_resolution_clock::now();
+      if(rv) {
+        rv = test_tar_pthread(candidate, victim);
+        //printf("verify result %d way = %d\n", rv, way);
+      } else {
+        if(residue == NULL) {
+          residue = candidate;
+          candidate = NULL;
+        } else {
+          candidate = append_list(candidate, residue);
+          residue = NULL;
+        }
+      }
+      if(rv) break;
+    } 
+    if(candidate) free_list(candidate);
+    if(residue) free_list(residue);
     free_list(victim);
-    if(rv) {
-      succ++;
-      if(way_pre == way)
-        keep++;
-      else if(keep > 0)
-        keep--;
-      else if(way <= way_pre)
-        way_pre = (way + way_pre) / 2;
-      else
-        way_pre++;
-    }
-    way = way_pre;
+    succ += rv;
     iter++;
-    printf("trials %d sucesses: %d keep: %d result: %d, way=%d\n", iter, succ, keep, rv, way);
+    long int time_all = std::chrono::duration_cast<std::chrono::milliseconds>(tend - tb1).count();
+    time_all_acc += time_all; 
+    long int time_trim = std::chrono::duration_cast<std::chrono::milliseconds>(tend - tb2).count();
+    time_trim_acc += time_trim;
+    printf("trials %d sucesses: %d result: %d (%ld, %ld)\n", iter, succ, rv, time_all, time_trim);
   }
-  printf("the predicted way = %d\n", way);
+  float ratio = (float)succ / iter;
+  time_all_acc /= iter;
+  time_trim_acc /= iter;
+  printf("sucess ratio: %f (%ld, %ld)\n", ratio, time_all_acc, time_trim_acc);
   return 0;
 }
